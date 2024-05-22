f77b41d8b29a46db86c569e6a84d7706cfc0c45d
Samuel Maciel <samuelmacielsts@gmail.com>
2024-05-22 17:48:13 -0300

    Feat: Initializing project

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, StyleSheet, Image } from "react-native"
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Image 
+          style={styles.logo}
+          source={require("./src/assets/logo.png")}
+        />
+      </View>
+      <View style={styles.principal_text}>
+        <Text>Personal Financial Manager</Text>
+      </View>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#F3F3FF",
+    justifyContent: "center",
+    alignItems: "center"
   },
-});
+
+  principal_text: {
+    padding: 50,
+  },
+
+  logo: {
+    width: 250, 
+    height: 250
+  }
+})
